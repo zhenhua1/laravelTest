f3f3ef2192e7e356419fe757b13de6292dc850aa
张振华 <2593481502@qq.com>
2019-11-28 10:35:40 +0800

    清空

--- routes/api/auth.php
+++
@@ -1,5 +0,0 @@
-<?php
-Route::post('login', 'AuthController@login');
-Route::post('logout', 'AuthController@logout');
-Route::post('refresh', 'AuthController@refresh');
-Route::post('user', 'AuthController@getUser');
